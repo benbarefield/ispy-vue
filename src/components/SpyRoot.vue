f3c965269ac78772b8d8cf1f28e2df3be4e29521
Ben Barefield <ben.barefield@gmail.com>
2024-04-13 13:53:23 -0700

    replay

--- src/components/SpyRoot.vue
+++ src/components/SpyRoot.vue
@@ -20,26 +20,41 @@
     {x: 150, y: 110, width: 30, height: 30, color: colors.orange},
   ];
 
-  const pick = shallowRef(shapes[Math.floor(Math.random() * shapes.length)]);
+  const playing = ref(true);
+  const pick = shallowRef(null);
   const selectResult = ref("");
   const hint = ref("");
 
-  const shapeSelected = (shape) => {
+  function shapeSelected(shape) {
+    if(!playing.value) {return;}
     if(shape === pick.value) {
       selectResult.value = "That's it!";
+      playing.value = false;
       return;
     }
 
     selectResult.value = rejections[Math.floor(Math.random() * rejections.length)];
     hint.value = Hints(shape, pick.value);
   };
+
+  function startGame() {
+    pick.value = shapes[Math.floor(Math.random() * shapes.length)];
+    playing.value = true;
+    hint.value = "";
+    selectResult.value = "";
+  };
+  startGame();
 </script>
 
 <template>
-  <div>
+  <div v-if="playing">
     <div>I spy... something <span :style="{ color: pick.color.value }">{{ pick.color.display }}</span></div>
     <div>{{selectResult}}</div>
     <div>{{hint}}</div>
+  </div>
+  <div v-if="!playing">
+    <div>{{selectResult}}</div>
+    <button @click="startGame">Try again</button>
   </div>
 
   <div class="spyObjectsContainer">
@@ -68,11 +83,25 @@
 
 .spyShape {
   position: absolute;
+  cursor: pointer;
 
   box-shadow: 0 0 10px rgba(190, 204, 77, 0);
   transition: box-shadow 0.2s;
 }
 .spyShape:hover {
-  box-shadow: 0 0 10px rgba(190, 204, 77, 0.75);
+  box-shadow: 0 0 10px rgba(234, 222, 100, 0.75);
+}
+
+button {
+  border: 1px solid var(--color-text);
+  color: var(--color-text);
+  background: var(--color-background);
+  cursor: pointer;
+  transition: border-color 0.2s;
+  box-sizing: border-box;
+  padding: 6px 12px;
+}
+button:hover {
+  border-color: var(--vt-c-indigo);
 }
 </style>
